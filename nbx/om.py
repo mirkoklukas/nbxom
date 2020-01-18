cbb8aa423f905f8f935e3ba7141cedff8f5586c5
Mirko <mirko.klukas@gmail.com>
2020-01-17 19:24:28 -0500

    I suck

--- nbx/om.py
+++ nbx/om.py
@@ -230,7 +230,7 @@ def chain_jobs(arrays, step):
     return s
 
 #Cell
-from pathlib import PosixPath as Path
+from pathlib import PurePosixPath as Path
 import pkg_resources
 import importlib
 from .templ import *
