4cc960e48fb31ea4c3b6040768356d0e9a2d845b
Mirko <mirko.klukas@gmail.com>
2020-01-17 20:03:14 -0500

    i am stupid

--- nbx/om.py
+++ nbx/om.py
@@ -302,7 +302,7 @@ class NbxBundle():
     def create_run_script(self, num, step, max_arr):
         path = self.path/'run.sh'
         with open(path, "w", newline="\n") as f:
-            f.write(r"#!/bin/sh\n\n")
+            f.write("#!/bin/sh\n\n")
             f.write(chain_jobs(get_arrays(num, max_arr), step))
 
     def create_script(self, tname, fname, vars):
